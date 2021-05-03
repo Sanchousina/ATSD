d1a74ebd1a89cc34e2564e5d6e3af1a6d93b424e
R2-D2-ПК\R2-D2 <ohotnikova.sasha2002@gmail.com>
2021-05-03 14:50:40 +0300

    Created new project HeapSort

---
+++ HeapSort_lb3/HeapSort_lb3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeapSort_lb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
