6b7d9f9ef25c08a5ed57faa56d9bdd5a50a24783
R2-D2-ПК\R2-D2 <ohotnikova.sasha2002@gmail.com>
2021-05-30 17:24:03 +0300

    added GenerateEmptyMatrix method

--- ATSD_Lab4/ATSD_Lab4/Graph.cs
+++ ATSD_Lab4/ATSD_Lab4/Graph.cs
@@ -6,5 +6,30 @@ namespace ATSD_Lab4
 {
     class Graph
     {
+        public int NumVertices { get; set; }
+
+        public bool Directed = false;
+
+        public int[,] Matrix { get; set; }
+
+        public Graph(int numVertices)
+        {
+            NumVertices = numVertices;
+
+            GenerateEmptyMatrix(numVertices);
+        }
+
+        private void GenerateEmptyMatrix(int num)
+        {
+            this.Matrix = new int[num, num];
+
+            for(int row = 0; row < num; row++)
+            {
+                for(int col = 0; col < num; col++)
+                {
+                    Matrix[row, col] = 0;
+                }
+            }
+        }
     }
 }
