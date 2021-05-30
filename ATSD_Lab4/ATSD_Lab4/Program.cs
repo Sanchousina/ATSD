1adab423b3a3342cacc6e2e5a7c76dc922920f57
R2-D2-ПК\R2-D2 <ohotnikova.sasha2002@gmail.com>
2021-05-30 19:06:45 +0300

    tested Dijkstra method

--- ATSD_Lab4/ATSD_Lab4/Program.cs
+++ ATSD_Lab4/ATSD_Lab4/Program.cs
@@ -6,13 +6,26 @@ namespace ATSD_Lab4
     {
         static void Main(string[] args)
         {
-            Graph graph = new Graph(4);
-            graph.AddEdge(0, 1);
-            graph.AddEdge(1, 2);
-            graph.AddEdge(1, 3);
-            graph.AddEdge(2, 3);
+            Graph graph = new Graph(9);
+            graph.AddEdge(0, 1, 4);
+            graph.AddEdge(0, 7, 8);
+            graph.AddEdge(1, 2, 8);
+            graph.AddEdge(1, 7, 11);
+            graph.AddEdge(2, 3, 7);
+            graph.AddEdge(2, 5, 4);
+            graph.AddEdge(2, 8, 2);
+            graph.AddEdge(3, 4, 9);
+            graph.AddEdge(3, 5, 14);
+            graph.AddEdge(4, 5, 10);
+            graph.AddEdge(5, 6, 2);
+            graph.AddEdge(6, 7, 1);
+            graph.AddEdge(6, 8, 6);
+            graph.AddEdge(7, 8, 7);
 
-            graph.PrintMatrix();
+
+            //graph.PrintMatrix();
+
+            graph.Dijkstra(0);
         }
     }
 }
